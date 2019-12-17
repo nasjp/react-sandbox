6b4030265f217aa1a96efe8e717665ffe5c9e0d6
nas <nas.silver.bullet@gmail.com>
2019-12-18 00:29:55 +0900

    :+1: Update about text

--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -5,6 +5,7 @@ const Home: FC<{}> = () => (
   <div className="container">
     <header className="header">nas</header>
     <p className="text">Software Engineer</p>
+    <p className="text">Backend, DevOps ...</p>
   </div>
 );
 
