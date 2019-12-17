f35c1c0588be6ba2487efdd9c88b008b8b6eaa7c
nas <nas.silver.bullet@gmail.com>
2019-12-17 22:17:21 +0900

    :+1: Remove unnessary class

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -4,13 +4,13 @@ import AppHeader from "./AppHeader";
 import "./App.css";
 
 const App: FC<{}> = () => (
-  <div className="container">
+  <>
     <AppHeader />
     <Switch>
       <Route path="/" />
       <Redirect to="/" />;
     </Switch>
-  </div>
+  </>
 );
 
 export default App;
